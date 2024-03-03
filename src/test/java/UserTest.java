22b64b0f6c3649ef9d7c7c47de2c27ea17065c2b
Noshin Nirvana Prachi <58959257+NoshinNirvana916Prachi@users.noreply.github.com>
2024-03-02 21:28:19 -0500

    add src

---
+++ src/test/java/UserTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class UserTest {
+    @Test
+    public void testUserConstructor() {
+        // Test user constructor and getter methods
+        User user = new User(1, "username", "password", "email");
+        assertEquals(1, user.getUserId());
+        assertEquals("username", user.getUsername());
+        assertEquals("password", user.getPassword());
+        assertEquals("email", user.getEmail());
+    }
+
+    // Add more test cases for User class as needed
+}
